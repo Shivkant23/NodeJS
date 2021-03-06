a5712d773526c5485a2f640b822fbc79edfa2bca
DESKTOP-STB5SS2\Lenovo <shivkant.sawarkar@customtravelsolutions.com>
2021-03-06 21:04:53 +0530

    changes in files used file.readdir method

--- course/module/file/app.js
+++ course/module/file/app.js
@@ -2,4 +2,9 @@ const fs = require('fs');
 //fs.access // as this method is non-blocking so we are usnig this method.
 
 const files = fs.readdirSync('../');
-console.log(files);
+// console.log(files);
+
+fs.readdir('./', (err, files) => {
+    if(err) console.log('Error', err);
+    else console.log('Result', files);
+});
